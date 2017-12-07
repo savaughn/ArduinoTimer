d63bc3e6a57801346ef61351f26120da26a1396c
savaughn <savaughn@uark.edu>
2017-12-07 02:25:23 -0600

    remove mutex implementation / comment & format code

--- arduinotimer.ino
+++ arduinotimer.ino
@@ -4,20 +4,21 @@
 
 LiquidCrystal lcd(7,8,9,10,11,12);
 
-int state = LOW;
-int inputPin = 2;
-int lastState = LOW;
-int mutex = 0;
+int state = LOW;      //Default LOW
+int inputPin = 2;     //Input location on board
+int lastState = LOW;  //Default LOW
 
 void setup(){
   
-  lcd.begin(16,2);
-  lcd.setCursor(0,0);
+  lcd.begin(16,2);               //Initialize LCD
+  lcd.setCursor(0,0);            //Initialize Cursor
   lcd.print("READY");  
   delay(1000);
   lcd.clear();
-  print(digitalRead(inputPin));
+  print(digitalRead(inputPin));  //Print initial status
 }
+
+//Print method defines states as HIGH or LOW
 void print(int state){
   lcd.clear();
   if (state == 0) 
@@ -27,35 +28,33 @@ void print(int state){
 }
 
 void loop(){
-     
-     state = digitalRead(inputPin);
-    
-     int buttonRelease = digitalRead(inputPin);
-     while(buttonRelease == state ){
-        buttonRelease = digitalRead(inputPin);
-      } 
-    
-    if (lastState == HIGH && state == LOW ){
-      state = HIGH;
-      mutex = 1;
-    }
-    else if (lastState == HIGH && state == HIGH ){
-      state = LOW; 
-      mutex = 0;      
+  //get current state of button (default LOW)  
+  state = digitalRead(inputPin);
+  
+  //make sure button was released
+  int buttonRelease = digitalRead(inputPin);
+  while(buttonRelease == state ){
+    buttonRelease = digitalRead(inputPin);
+  } 
+  
+  //Keeps state HIGH until button is pushed
+  if (lastState == HIGH && state == LOW ){
+    state = HIGH;
   }
-    
-    
-    if (state != lastState){      
-      print(state); 
-     
-      delay(00);       
-    } 
-      
-      lastState = state;
-      
-      
-        
+  
+  //Returns to LOW when button is pushed when already in HIGH state
+  else if (lastState == HIGH && state == HIGH ){
+    state = LOW;    
   }
+  
+  //updates LCD when state change occurs
+  if (state != lastState){      
+    print(state);         
+  } 
+  
+  //track last state
+  lastState = state;
+}
 
   
 
