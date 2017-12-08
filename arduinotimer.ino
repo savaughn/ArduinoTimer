b4248e58ce21c0364c257f4bc7db1f1318d9599f
savaughn <savaughn@uark.edu>
2017-12-07 21:57:43 -0600

    comments and reorganization

--- arduinotimer.ino
+++ arduinotimer.ino
@@ -35,25 +35,28 @@ void print(int state){
 }
 
 void loop(){
-   while (reset == 0){
+   //Initial state LOW mandatory hold HIGH 1 second before state change to HIGH
+    while (state == LOW && reset == 1){
+       while (digitalRead(inputPin) == HIGH){
+        lcd.setCursor(0,0);
+         lcd.print(holdCount++);    //TODO: LED instead of count print
+       }
+        if(holdCount < 1001){      //Button wasn't held long enough
+          holdCount = 0;           //reset state change timer
+      } if (holdCount > 999){      //Button was held long enough
+        state = HIGH;              //Change state
+        holdCount = 0;
+      }
+    }
+    
+    //Busy wait if from HIGH to LOW the button has not been let go
+    while (reset == 0){
       while (digitalRead(inputPin) == HIGH){
         //do nothing
       }
       reset = 1;         
    }
   
-    while (state == LOW && reset == 1){
-       while (digitalRead(inputPin) == HIGH){
-        lcd.setCursor(0,0);
-        lcd.clear();
-         lcd.print(holdCount++);
-       }
-        if(holdCount < 1001){
-          holdCount = 0;        
-      } if (holdCount > 999){
-        state = HIGH;         
-      }
-    }
     
     //Keeps state HIGH until button is pushed
     if (lastState == HIGH && digitalRead(inputPin) == LOW ){
@@ -63,7 +66,7 @@ void loop(){
     //Returns to LOW when button is pushed when already in HIGH state
     else if (lastState == HIGH && state == HIGH ){
       state = LOW;
-      reset = 0;
+      reset = 0;        //prevents state change timer from starting from HIGH to LOW
     }
     
     //updates LCD when state change occurs
@@ -73,14 +76,10 @@ void loop(){
     
     //track last state
     lastState = state;
-    holdCount = 0;
-
-  
 }
 
 /****************************************************************
-TODO: 1. Add mandatory hold period in transition from LOW to HIGH
-      2. Add LED HIGH/LOW indicators
+TODO: 2. Add LED HIGH/LOW indicators
       3. Implement Timer
       4. Display previous time
       5. Implement scramble page  
